9afd278ea00a6b4eebf3fc5087ce2df19e4e8bcd
carlosbarrientosarias27-star <carlos.barrientosarias27@gmail.com>
2026-03-28 10:31:03 +0100

    añadir código utils

---
+++ app/utils.py
@@ -0,0 +1,3 @@
+def formatear_moneda(cantidad):
+    """Formatea un número como moneda con el símbolo $."""
+    return f"${cantidad:,.2f}"
--- calculadora_propina.py
+++ calculadora_propina.py
@@ -1,8 +1,6 @@
 
 
-def formatear_moneda(cantidad):
-    """Formatea un número como moneda con el símbolo $."""
-    return f"${cantidad:,.2f}"
+
 
 def menu_interactivo():
     """
